602325fb9bbcd29d1584db02fd4e809adef182ee
Prasad Patil <prasad.patil.c@gmail.com>
2020-06-06 15:02:54 +0300

    Creates the capitals feature class with the fields

--- createEGDB.py
+++ createEGDB.py
@@ -28,6 +28,23 @@ def createEGDBConnectionFile():
 
 def createCapitalFC():
     print("Creating Capitals Feature Class")
+    #Create feature class
+    out_path = "sa_connection_to_edmar_test.sde"
+    out_name = "capitals"
+    geometry_type = "POINT"
+    template = None
+    has_m = "DISABLED"
+    has_z = "DISABLED"
+    spatial_ref = arcpy.SpatialReference(3857)
+    arcpy.CreateFeatureclass_management(out_path,out_name,geometry_type,template,has_m,has_z,spatial_ref)
+
+    #Add fields
+    arcpy.AddField_management(out_path + "\\" + out_name, field_name="country", field_type="TEXT", field_precision="", field_scale="", field_length="50", field_alias="Country", field_is_nullable="NULLABLE", field_is_required="NON_REQUIRED", field_domain="")
+    arcpy.AddField_management(out_path + "\\" + out_name, field_name="capital", field_type="TEXT", field_precision="", field_scale="", field_length="50", field_alias="Capital", field_is_nullable="NULLABLE", field_is_required="NON_REQUIRED", field_domain="")
+    arcpy.AddField_management(out_path + "\\" + out_name, field_name="latitude", field_type="DOUBLE", field_precision="", field_scale="", field_length="", field_alias="Latitude", field_is_nullable="NULLABLE", field_is_required="NON_REQUIRED", field_domain="")
+    arcpy.AddField_management(out_path + "\\" + out_name, field_name="longitude", field_type="DOUBLE", field_precision="", field_scale="", field_length="", field_alias="Longitude", field_is_nullable="NULLABLE", field_is_required="NON_REQUIRED", field_domain="")
+    
+
 
 if __name__ == '__main__':
     createEGDB()
