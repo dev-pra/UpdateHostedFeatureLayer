64934e800899743d79b70f4d1276cade1585a374
Prasad Patil <prasad.patil.c@gmail.com>
2020-06-06 14:35:21 +0300

    Creates a connection file for the created geodatabase

--- createEGDB.py
+++ createEGDB.py
@@ -13,7 +13,19 @@ def createEGDB():
     arcpy.CreateEnterpriseGeodatabase_management(database_platform,instance_name,database_name,account_authentication,database_admin,database_admin_password,sde_schema,None,None,None,authorization_file)
 
 def createEGDBConnectionFile():
+    print("Creating connection file in the local directory")
+    out_folder_path = "."
+    out_name = "sa_connection_to_edmar_test"
+    database_platform = "SQL_SERVER"
+    instance = "localhost"
+    account_authentication = "DATABASE_AUTH"
+    username = "sa"
+    password = "Password_01"
+    save_user_pass = "SAVE_USERNAME"
+    database = "edmar_test"
     
+    arcpy.CreateDatabaseConnection_management(out_folder_path,out_name,database_platform,instance,account_authentication,username,password,save_user_pass,database)
 
 if __name__ == '__main__':
     createEGDB()
+    createEGDBConnectionFile()
